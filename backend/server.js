97e8048aad5c885edd27576876cebb548243b677
suri193 <kr.surndra101@gmail.com>
2025-06-02 01:01:53 +0530

    cors middleware added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats } = require("./Data/data");
+const cors = require("cors");
+const { chats } = require("./data/data");
 
 
 const app = express()
 dotenv.config();
 
+app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true  
+}));
+  
 app.get("/", (req, res) => {
     res.send("API is runningcsuccs")
 });
