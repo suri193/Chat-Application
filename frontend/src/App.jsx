55e077b8abc3e4195add6965758d2c5cee9ea6f2
suri193 <kr.surndra101@gmail.com>
2025-06-02 01:05:14 +0530

    Chatpage and Homepage components created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import { Button, HStack } from "@chakra-ui/react"
- 
- export default function App() {
-   return (
-     <div>
-       
-     </div>
-   )
- }
- 
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatpage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/chats" element={<Chatpage />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ frontend/src/Pages/Chatpage.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Chatpage = () => {
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    try {
+      const { data } = await axios.get('/api/chat');
+      setChats(data);
+    } catch (error) {
+      console.error("Error fetching chats:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div key = {chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Chatpage;
