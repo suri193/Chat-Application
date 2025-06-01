21dda26176184251e7839175fedcaa292528b1da
suri193 <kr.surndra101@gmail.com>
2025-06-02 00:59:54 +0530

    proxy sever added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,7 @@ import { Button, HStack } from "@chakra-ui/react"
  export default function App() {
    return (
      <div>
-       <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+       
      </div>
    )
  }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  server:{
+    proxy:{
+        '/api':{
+           target:'http://localhost:7000',
+           secure:false,
+        },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
